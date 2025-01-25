a439ec645dde4602afe324720c0e15dd7024f242
AbduLaziz <l.komilov@axonlogic.uz>
2025-01-25 19:48:53 +0500

    first commit

---
+++ src/main/java/dasturlashuz/giybat/GiybatApplication.java
@@ -0,0 +1,13 @@
+package dasturlashuz.giybat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiybatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiybatApplication.class, args);
+	}
+
+}

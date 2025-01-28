8f948c3b47ce56436006ab321b41e24f03c06faf
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-01-28 15:39:32 +0500

    Added: InvalidPasswordException

---
+++ src/main/java/dasturlashuz/giybat/exceptions/InvalidPasswordException.java
@@ -0,0 +1,7 @@
+package dasturlashuz.giybat.exceptions;
+
+public class InvalidPasswordException extends RuntimeException{
+    public InvalidPasswordException(String message) {
+        super(message);
+    }
+}

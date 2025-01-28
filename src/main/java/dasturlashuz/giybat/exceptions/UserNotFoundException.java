d04eb41cdd687de8531e80d857c13c7eff3c8a9a
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-01-28 21:23:23 +0500

    Added: UserNotFoundException

---
+++ src/main/java/dasturlashuz/giybat/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package dasturlashuz.giybat.exceptions;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}

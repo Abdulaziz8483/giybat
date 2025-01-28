c598f0fcf006831d9c0e7ef71dde0830f0d047cc
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-01-28 21:21:58 +0500

    Added: *URD API

--- src/main/java/dasturlashuz/giybat/controller/ProfileController.java
+++ src/main/java/dasturlashuz/giybat/controller/ProfileController.java
@@ -4,10 +4,9 @@ import dasturlashuz.giybat.dto.profile.ProfileCreateDTO;
 import dasturlashuz.giybat.service.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/profile")
@@ -18,5 +17,20 @@ public class ProfileController {
     @PostMapping("")
     public ResponseEntity<ProfileCreateDTO.ProfileResponse> create(@RequestBody ProfileCreateDTO profileDTO) {
         return ResponseEntity.ok(service.create(profileDTO));
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<ProfileCreateDTO.ProfileResponse>> getAll(){
+        return ResponseEntity.ok(service.getAll());
+    }
+
+    @PutMapping("/{profileId}")
+    public ResponseEntity<ProfileCreateDTO.ProfileResponse>update(@PathVariable Long profileId, @RequestBody ProfileCreateDTO profileDTO) {
+        return ResponseEntity.ok(service.update(profileId, profileDTO));
+    }
+
+    @DeleteMapping("/{profileId}")
+    public ResponseEntity<String> delete(@PathVariable Long profileId) {
+        return ResponseEntity.ok(service.delete(profileId));
     }
 }

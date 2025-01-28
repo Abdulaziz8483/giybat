f93df21bb2d56fafb957c4afc7b4bd7cd313eff4
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-01-28 15:40:11 +0500

    Added: done create method

--- src/main/java/dasturlashuz/giybat/service/ProfileService.java
+++ src/main/java/dasturlashuz/giybat/service/ProfileService.java
@@ -4,18 +4,30 @@ import dasturlashuz.giybat.dto.StandardResponse;
 import dasturlashuz.giybat.dto.profile.ProfileCreateDTO;
 import dasturlashuz.giybat.entity.Profile;
 import dasturlashuz.giybat.exceptions.EmailOrPhoneAlreadyExistsException;
+import dasturlashuz.giybat.exceptions.InvalidPasswordException;
 import dasturlashuz.giybat.exceptions.MissingRequiredFieldsException;
+import dasturlashuz.giybat.mapper.profile.ProfileMapper;
 import dasturlashuz.giybat.repository.ProfileRepository;
+import dasturlashuz.giybat.util.profile.ProfileUtil;
 import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+import static dasturlashuz.giybat.util.profile.ProfileUtil.checkPasswordValid;
 
 @Service
 public class ProfileService {
     @Autowired
     ProfileRepository repository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    ProfileMapper profileMapper;
 
-   /* public ProfileCreateDTO.ProfileResponse create(ProfileCreateDTO profileDTO) {
+    public ProfileCreateDTO.ProfileResponse create(ProfileCreateDTO profileDTO) {
         StandardResponse standardResponse = new StandardResponse("Email yoki Phone kiritilmadi", false);
         if (profileDTO.email() == null && profileDTO.phone() == null) {
             throw new MissingRequiredFieldsException(standardResponse.message());
@@ -28,10 +40,22 @@ public class ProfileService {
             standardResponse = checkPhoneExists(profileDTO.phone());
             if (!standardResponse.status()) throw new EmailOrPhoneAlreadyExistsException(standardResponse.message());
         }
-        Profile profile = new Profile();
-        profile.
+        //static import
+        if (!checkPasswordValid(profileDTO.password()).status()) throw new InvalidPasswordException(checkPasswordValid(profileDTO.password()).message());
 
-    }*/
+
+        Profile profile = new Profile();
+        profile.setName(profileDTO.name());
+        profile.setEmail(profileDTO.email());
+        profile.setPhone(profileDTO.phone());
+        profile.setStatus(profileDTO.status());
+        profile.setVisible(true);
+        profile.setCreatedAt(LocalDateTime.now());
+        profile.setPassword(passwordEncoder.encode(profileDTO.password()));
+        repository.save(profile);
+
+        return profileMapper.profileToProfileDTO(profile);
+    }
 
 
 

f47e26d7cba84fcf8829329c0eae9cfec02516bb
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-01-28 15:40:31 +0500

    Added: method checkPasswordUtil

--- src/main/java/dasturlashuz/giybat/util/profile/ProfileUtil.java
+++ src/main/java/dasturlashuz/giybat/util/profile/ProfileUtil.java
@@ -1,5 +1,13 @@
 package dasturlashuz.giybat.util.profile;
 
+import dasturlashuz.giybat.dto.StandardResponse;
+
 public class ProfileUtil {
 
+    public static StandardResponse checkPasswordValid(String password) {
+        if (password.length() < 6) {
+            return new StandardResponse("Password must be at least 6 characters long", false);
+        }
+        return new StandardResponse("Ok", true);
+    }
 }

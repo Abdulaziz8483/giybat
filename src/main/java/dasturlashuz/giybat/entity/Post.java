dd590b89bb7cce19f7c9cb24707cabcd7469b8cd
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-01-30 00:04:41 +0500

    Added: Post fill field and PostStatus

--- src/main/java/dasturlashuz/giybat/entity/Post.java
+++ src/main/java/dasturlashuz/giybat/entity/Post.java
@@ -1,5 +1,44 @@
 package dasturlashuz.giybat.entity;
 
+import dasturlashuz.giybat.enums.PostStatus;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "post")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Setter
+@Getter
 public class Post {
+     @Id
+     @GeneratedValue(strategy = GenerationType.UUID)
+     String id;
+
+     @Column(nullable = false)
+     String title;
+
+     @Column(nullable = false)
+     String content;
+
+     @Column(nullable = false)
+     Long profileId;
+
+     @Column(nullable = false)
+     LocalDateTime createdAt;
+
+     @Column
+     LocalDateTime updatedAt;
+
+     @Column(nullable = false)
+     PostStatus status;
+
+     @Column(nullable = false)
+     Boolean visible;
+
 
 }

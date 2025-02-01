782bcfe49dd955ac4ab896fc9ffde0d9cd8e3165
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-02-01 23:51:24 +0500

    Added: AttachRepository

---
+++ src/main/java/dasturlashuz/giybat/repository/AttachRepository.java
@@ -0,0 +1,7 @@
+package dasturlashuz.giybat.repository;
+
+import dasturlashuz.giybat.entity.AttachEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AttachRepository extends JpaRepository<AttachEntity,String> {
+}

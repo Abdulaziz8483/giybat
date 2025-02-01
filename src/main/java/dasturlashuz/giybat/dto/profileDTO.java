b270f40f7154d2554578e6bcf4d0314f730274e8
AbduLaziz <l.komilov@axonlogic.uz>
2025-02-01 15:53:05 +0500

    Add a little change

--- src/main/java/dasturlashuz/giybat/dto/profileDTO.java
+++ src/main/java/dasturlashuz/giybat/dto/profileDTO.java
@@ -1,4 +1,20 @@
 package dasturlashuz.giybat.dto;
 
+import dasturlashuz.giybat.enums.ProfileRole;
+import dasturlashuz.giybat.enums.ProfileStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class profileDTO {
+    private Long id;
+    private String name;
+    private String surname;
+    private String email;
+    private String phone;
+    private String password;
+    private ProfileStatus status;
+    private ProfileRole role;
+    private String jwtToken;
 }
--- src/main/java/dasturlashuz/giybat/repository/ProfileRepository.java
+++ src/main/java/dasturlashuz/giybat/repository/ProfileRepository.java
@@ -1,10 +1,22 @@
 package dasturlashuz.giybat.repository;
 
-import dasturlashuz.giybat.entity.Profile;
+import dasturlashuz.giybat.entity.ProfileEntity;
+import dasturlashuz.giybat.enums.ProfileStatus;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
-public interface ProfileRepository extends JpaRepository<Profile, Long> {
+import java.util.Optional;
 
-    boolean existsByEmailAndVisibleTrue(String email);
+public interface ProfileRepository extends JpaRepository<ProfileEntity, Long> {
+
+    Optional<ProfileEntity> findByUsernameAndVisibleTrue(String username);
+    Optional<ProfileEntity> findByIdAndVisibleTrue(Integer profileId);
+
     boolean existsByPhoneAndVisibleTrue(String phone);
+    @Transactional
+    @Modifying
+    @Query("update ProfileEntity set status=?2 where id=?1")
+    void changeStatus(Integer profileId, ProfileStatus status);
 }

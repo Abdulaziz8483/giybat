86d33e56b466360a1df3351be5cd479f77add69e
AbduLaziz <l.komilov@axonlogic.uz>
2025-02-01 15:52:45 +0500

    Add util

---
+++ src/main/java/dasturlashuz/giybat/util/JwtUtil.java
@@ -0,0 +1,52 @@
+package dasturlashuz.giybat.util;
+
+
+
+
+import dasturlashuz.giybat.dto.JwtDTO;
+import dasturlashuz.giybat.entity.ProfileRoleEntity;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtUtil {
+    private static final int tokenLiveTime = 1000 * 3600 * 24;
+    private static final String secretKey = "veryLongSecretmazgillattayevlasharaaxmojonjinnijonsurbetbekkiydirhonuxlatdibekloxovdangasabekochkozjonduxovmashaynikmaydagapchishularnioqiganbolsangizgapyoqaniqsizmazgi";
+
+    public static String encode(String username, Integer id, ProfileRoleEntity roles) {
+        Map<String, String> Claims = new HashMap<>();
+        Claims.put("username", username);
+        Claims.put("id", String.valueOf(id));
+        Claims.put("roles", roles.getRoles().name());
+
+        return Jwts
+                .builder()
+                .claims(Claims)
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + tokenLiveTime))
+                .signWith(getSignInKey())
+                .compact();
+    }
+    public static JwtDTO decode(String token) {
+        Claims claims = Jwts
+                .parser()
+                .verifyWith(getSignInKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+        String username = (String) claims.get("username");
+        String role = (String) claims.get("roles");
+        return new JwtDTO(role, username);
+    }
+    private static SecretKey getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}

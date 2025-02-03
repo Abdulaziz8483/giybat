534556f30cf7f926c1dccf6fb0250b798ae3b01b
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-02-04 00:34:35 +0500

    Added: name changed Post from PostEntity to

--- src/main/java/dasturlashuz/giybat/entity/PostEntity.java
+++ src/main/java/dasturlashuz/giybat/entity/PostEntity.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Setter
 @Getter
-public class Post {
+public class PostEntity {
      @Id
      @GeneratedValue(strategy = GenerationType.UUID)
      String id;

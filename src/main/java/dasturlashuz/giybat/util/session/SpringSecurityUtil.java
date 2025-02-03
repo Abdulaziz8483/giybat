ba0f2f2f721ce2a05e1a31b429d98ba4eea73955
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-02-04 00:35:06 +0500

    Added: SpringSecurityUtil for get current user id

---
+++ src/main/java/dasturlashuz/giybat/util/session/SpringSecurityUtil.java
@@ -0,0 +1,22 @@
+package dasturlashuz.giybat.util.session;
+
+import dasturlashuz.giybat.config.CustomUserDetails;
+import dasturlashuz.giybat.exceptions.AppBadException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class SpringSecurityUtil {
+    public static Integer getCurrentUserId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            Object principal = authentication.getPrincipal();
+            if (principal instanceof CustomUserDetails) {
+                return ((CustomUserDetails) principal).getId();
+            }else if (principal instanceof String && "anonymousUser".equals(((String) principal).trim())) {
+                throw new RuntimeException("Anonymous user");
+            }
+        }
+        throw new AppBadException("http://localhost:8081/api/v1/auth/login");
+    }
+}

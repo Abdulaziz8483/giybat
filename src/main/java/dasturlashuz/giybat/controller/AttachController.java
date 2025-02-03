73c025057e598ecdbd70f7431ac5991ed78b538b
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-02-04 00:32:04 +0500

    Added: getAll and delete and open API

--- src/main/java/dasturlashuz/giybat/controller/AttachController.java
+++ src/main/java/dasturlashuz/giybat/controller/AttachController.java
@@ -1,15 +1,16 @@
 package dasturlashuz.giybat.controller;
 
 import dasturlashuz.giybat.dto.attach.AttachResponse;
+import dasturlashuz.giybat.entity.AttachEntity;
 import dasturlashuz.giybat.service.AttachService;
 import dasturlashuz.giybat.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/attach")
@@ -18,7 +19,25 @@ public class AttachController {
     private final AttachService attachService;
 
     @PostMapping("/upload")
-    public ResponseEntity<AttachResponse> upload(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<AttachResponse.AttachUrl> upload(@RequestParam("file") MultipartFile file) {
         return ResponseEntity.ok(attachService.upload(file));
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<AttachResponse>> getAll() {
+        return ResponseEntity.ok(attachService.getAll());
+    }
+
+    /*@PutMapping("/{attachId}")
+    public ResponseEntity<AttachResponse> update(@PathVariable Long attachId, @RequestBody AttachEntity attachEntity) {}*/
+
+    @DeleteMapping("/{attachId}")
+    public ResponseEntity<String> delete(@PathVariable String attachId) {
+        return ResponseEntity.ok(attachService.delete(attachId));
+    }
+
+    @GetMapping("/open/{attachId}")
+    public ResponseEntity<Resource> open(@PathVariable String attachId) {
+        return attachService.open(attachId);
     }
 }

8d529b6386312a2b463f22713828f92d2dc11339
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-02-04 17:40:44 +0500

    Added: PostCreateDTO and PostCreateDTO.PostResponse

---
+++ src/main/java/dasturlashuz/giybat/dto/post/PostCreateDTO.java
@@ -0,0 +1,21 @@
+package dasturlashuz.giybat.dto.post;
+
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDateTime;
+
+public record PostCreateDTO(
+        String title,
+        @Size(min = 100)
+        String content,
+        String photoId
+) {
+    public record PostResponse(
+            String id,
+            String title,
+            String content,
+            String photoUrl,
+            String photoId,
+            LocalDateTime createdDate
+    ){}
+}

30c1b38dfb27334771ea5b6917ad5c14d60ec128
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-02-04 17:40:09 +0500

    Added: create API

--- src/main/java/dasturlashuz/giybat/controller/PostController.java
+++ src/main/java/dasturlashuz/giybat/controller/PostController.java
@@ -1,9 +1,13 @@
 package dasturlashuz.giybat.controller;
 
+import dasturlashuz.giybat.dto.post.PostCreateDTO;
 import dasturlashuz.giybat.service.PostService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class PostController {
     private final PostService postService;
+
+    @PostMapping("")
+    public ResponseEntity<PostCreateDTO.PostResponse> create(@RequestBody @Valid PostCreateDTO dto) {
+        return ResponseEntity.ok(postService.create(dto));
+    }
 
 }

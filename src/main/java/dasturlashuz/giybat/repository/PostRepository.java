f8b9cd64c0e99c8e047bdcec8e6bf2922bd2b028
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-02-05 22:16:35 +0500

    Added:  findByProfileId

--- src/main/java/dasturlashuz/giybat/repository/PostRepository.java
+++ src/main/java/dasturlashuz/giybat/repository/PostRepository.java
@@ -1,8 +1,19 @@
 package dasturlashuz.giybat.repository;
 
 
+import dasturlashuz.giybat.dto.post.PostCreateDTO;
 import dasturlashuz.giybat.entity.PostEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
-public interface PostRepository extends JpaRepository<PostEntity, String> {
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<PostEntity, String>, JpaSpecificationExecutor<PostEntity> {
+
+    List<PostEntity> findByProfileId(Integer profileId);
+    Page<PostEntity> findByProfileId(Integer profileId, Pageable pageable);
 }

dcc0ef4b72ca89a5a523492b0e98997c4a900408
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-02-05 22:11:38 +0500

    Added:  update and delete, get all, get pagination, get by id, filter API

--- src/main/java/dasturlashuz/giybat/controller/PostController.java
+++ src/main/java/dasturlashuz/giybat/controller/PostController.java
@@ -5,11 +5,12 @@ import dasturlashuz.giybat.service.PostService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/post")
@@ -22,4 +23,43 @@ public class PostController {
         return ResponseEntity.ok(postService.create(dto));
     }
 
+    @GetMapping("")
+    public ResponseEntity<List<PostCreateDTO.PostResponse>> getAll() {
+        return ResponseEntity.ok(postService.getAll());
+    }
+
+    @PutMapping("/{postId}")
+    public ResponseEntity<PostCreateDTO.PostResponse> update(@PathVariable String postId, @RequestBody @Valid PostCreateDTO dto) {
+        return ResponseEntity.ok(postService.update(postId, dto));
+    }
+
+    @DeleteMapping("/{postId}")
+    public ResponseEntity<String> delete(@PathVariable String postId) {
+        return ResponseEntity.ok(postService.delete(postId));
+    }
+
+    @GetMapping("/get-all-posts-owner")
+    public ResponseEntity<List<PostCreateDTO.PostResponse>> getAllPostsOwner() {
+        return ResponseEntity.ok(postService.getAllPostsOwner());
+    }
+
+    @GetMapping("/get-all-posts-pagination")
+    public ResponseEntity<PageImpl<PostCreateDTO.PostResponse>> getAllPostsPagination(
+            @RequestParam(value = "page", defaultValue = "1") int page,
+            @RequestParam(value = "size", defaultValue = "5") int size) {
+        return ResponseEntity.ok().body(postService.getAllPostsPagination(page, size));
+    }
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<PostCreateDTO.PostResponse> getById(@PathVariable String postId) {
+        return ResponseEntity.ok(postService.getById(postId));
+    }
+
+    @GetMapping("/filter")
+    public ResponseEntity<List<PostCreateDTO.PostFilterResponse>> filter(
+            @RequestBody  PostCreateDTO.PostFilterDTO dto,
+            @RequestParam(value = "page", defaultValue = "1") int page,
+            @RequestParam(value = "size", defaultValue = "5") int size){
+        return ResponseEntity.ok(postService.filter(dto, page-1, size));
+    }
 }

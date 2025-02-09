e17ac0fdb17b5d3bd232dce2e2455878101819ab
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-02-09 23:03:24 +0500

    Added: native query get profile detail

--- src/main/java/dasturlashuz/giybat/repository/ProfileRepository.java
+++ src/main/java/dasturlashuz/giybat/repository/ProfileRepository.java
@@ -2,10 +2,12 @@ package dasturlashuz.giybat.repository;
 
 import dasturlashuz.giybat.entity.ProfileEntity;
 import dasturlashuz.giybat.enums.ProfileStatus;
+import dasturlashuz.giybat.mapper.profile.ProfileShortInfoMapper;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -15,8 +17,21 @@ public interface ProfileRepository extends JpaRepository<ProfileEntity, Integer>
     Optional<ProfileEntity> findByIdAndVisibleTrue(Integer profileId);
     boolean existsByUsernameAndVisibleTrue(String username);
 
+    @Query(value = "SELECT p.name AS name, " +
+            "p.user_name AS username, " +
+            "p.attach_id AS photoId, " +
+            "STRING_AGG(pr.roles, ',') AS roles " +  // Probellar to'g'ri qo'shildi
+            "FROM profiles AS p " +  // profiles jadvali qo'shtirnoqsiz
+            "JOIN profile_role pr ON pr.profile_id = p.id " +
+            "WHERE p.id = ?1 " +
+            "GROUP BY p.id, p.name, p.user_name, p.attach_id",
+            nativeQuery = true)
+    ProfileShortInfoMapper getProfileShortInfoMapper(Integer profileId);
+
     @Transactional
     @Modifying
     @Query("update ProfileEntity set status=?2 where id=?1")
     void changeStatus(Integer profileId, ProfileStatus status);
+
+
 }

6984790f82fa38da6ed89e554b7ec54e8d4942f0
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-02-10 18:08:23 +0500

    Added: extend JpaSpecification

--- src/main/java/dasturlashuz/giybat/repository/ProfileRepository.java
+++ src/main/java/dasturlashuz/giybat/repository/ProfileRepository.java
@@ -5,13 +5,14 @@ import dasturlashuz.giybat.enums.ProfileStatus;
 import dasturlashuz.giybat.mapper.profile.ProfileShortInfoMapper;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface ProfileRepository extends JpaRepository<ProfileEntity, Integer> {
+public interface ProfileRepository extends JpaRepository<ProfileEntity, Integer>, JpaSpecificationExecutor<ProfileEntity> {
 
     Optional<ProfileEntity> findByUsernameAndVisibleTrue(String username);
     Optional<ProfileEntity> findByIdAndVisibleTrue(Integer profileId);
